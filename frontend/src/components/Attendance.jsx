f63e5b0ce1ec00ca49b47f94e2a224ae79de840a
Your Name <you@example.com>
2025-01-02 14:31:11 +0530

    Changed

---
+++ frontend/src/components/Attendance.jsx
@@ -0,0 +1,65 @@
+import { CiMenuKebab } from 'react-icons/ci';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        name: 'Mon',
+        present: 40,
+        absent: 24,
+    },
+    {
+        name: 'Tue',
+        present: 50,
+        absent: 14,
+    },
+    {
+        name: 'Wed',
+        present: 55,
+        absent: 9,
+    },
+    {
+        name: 'Thu',
+        present: 45,
+        absent: 19,
+    },
+    {
+        name: 'Fri',
+        present: 35,
+        absent: 29,
+    },
+];
+function AttendanceChart() {
+    return (
+        <div className='bg-white rounded-md shadow-md p-4 min-w-[400px] h-full flex-1'>
+            {/* Title */}
+            <div className='flex justify-between items-center'>
+                <h1 className='text-2xl font-semibold'>Attendance</h1>
+                <CiMenuKebab className='rotate-90 text-4xl text-gray-500' />
+            </div>
+            {/* Chart */}
+            <ResponsiveContainer minWidth="450px" minHeight="400px">
+                <BarChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+
+                >
+                    <CartesianGrid strokeDasharray="4" strokeOpacity={"0.4"} />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend align="left" verticalAlign="top" />
+                    <Bar dataKey="present" fill="#8884d8" />
+                    <Bar dataKey="absent" fill="#82ca9d" />
+                </BarChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
+export default AttendanceChart
---
+++ frontend/src/components/FinanceGraphChart.jsx
@@ -0,0 +1,97 @@
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+const data = [
+    {
+        name: 'JAN',
+        income: 40000,
+        spend: 24000,
+    },
+    {
+        name: 'FEB',
+        income: 60000,
+        spend: 40000,
+    },
+    {
+        name: 'MAR',
+        income: 30000,
+        spend: 20000,
+    },
+    {
+        name: 'APR',
+        income: 55000,
+        spend: 35000,
+    },
+    {
+        name: 'MAY',
+        income: 70000,
+        spend: 30000,
+    },
+    {
+        name: 'JUNE',
+        income: 80000,
+        spend: 45000,
+    },
+    {
+        name: 'JULY',
+        income: 20000,
+        spend: 10000,
+    },
+    {
+        name: 'AUG',
+        income: 20000,
+        spend: 10000,
+    },
+    {
+        name: 'SEPT',
+        income: 20000,
+        spend: 10000,
+    },
+    {
+        name: 'OCT',
+        income: 20000,
+        spend: 10000,
+    },
+    {
+        name: 'NOV',
+        income: 20000,
+        spend: 10000,
+    },
+    {
+        name: 'DEC',
+        income: 20000,
+        spend: 10000,
+    },
+];
+function FinanceGraphChart() {
+    return (
+        <div className='w-full h-full'>
+            {/* Titile */}
+            <div>
+                <h1 className='text-2xl font-semibold'>Finance</h1>
+            </div>
+            {/* Chart */}
+            <ResponsiveContainer minWidth="inherit" minHeight="400px">
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line type="monotone" dataKey="income" stroke="#8884d8" activeDot={{ r: 8 }} />
+                    <Line type="monotone" dataKey="spend" stroke="#82ca9d" />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
+    )
+}
+export default FinanceGraphChart
